fa42f411ed3ab7af33823ba596012f8f0cf194e5
Gustavo <araujogustavo602@gmail.com>
2023-11-28 10:22:19 -0300

    academia

---
+++ pastaclasse/usuario.php
@@ -0,0 +1,130 @@
+<?php
+
+    class Usuario
+    {
+
+        private $nome;
+        private $emai;
+        private $dat;
+        private 
+
+        public function __construct(){}
+
+        public function create($_nome, $_valor, $_categoria)
+        {
+            $this->nome = $_nome;
+            $this->valor = $_valor;
+            $this->categoria = $_categoria;
+        }
+
+        public function getNome(){
+            return $this->nome;
+        }
+
+        public function getValor()
+        {
+            return $this->valor;
+        }
+
+        public function getCategoria()
+        {
+            return $this->categoria;
+        }
+
+        public function setNome($_nome)
+        {
+            $this->nome = $_nome;
+        }
+
+        public function setValor($_valor)
+        {
+            $this->valor = $_valor;
+        }
+
+        public function setCategoria($_categoria)
+        {
+            $this->categoria = $_categoria;
+        }
+
+        public function inserirProduto()
+        {
+            include("assets/db/conn.php");
+            $sql = "CALL piProduto(:nome, :valor, :categoria)";
+
+            $data = [
+                'nome' => $this->nome,
+                'valor' => $this->valor,
+                'categoria' => $this->categoria
+            ];
+
+            $statement = $conn->prepare($sql);
+            $statement->execute($data);
+
+            return true;
+
+        }
+
+        public function listarProduto()
+        {
+            include("assets/db/conn.php");
+
+            $sql = "CALL psListarProduto('')";
+            $data = $conn->query($sql)->fetchAll();
+
+            return $data;
+
+      
+        }
+
+        public function buscarProduto($_id)
+        {
+            include("assets/db/conn.php");
+
+            $sql = "CALL psProduto('$_id')";
+            $data = $conn->query($sql)->fetchAll();
+
+            foreach ($data as $item) {
+                $this->nome = $item["nomeProduto"];
+                $this->valor = $item["valorUnitario"];
+                $this->categoria = $item["nomeCategoria"];
+            }
+
+            return true;
+
+        }
+
+        public function excluirProduto($_id)
+        {
+            include("assets/db/conn.php");
+            $sql = "CALL pdProduto(:id)";
+
+            $data = [
+                'id' => $_id
+            ];
+
+            $statement = $conn->prepare($sql);
+            $statement->execute($data);
+
+            return true;
+        }
+
+        public function atualizarProduto($_id)
+        {
+            include("assets/db/conn.php");
+            $sql = "CALL puProduto(:id, :nome, :valor)";
+
+            $data = [
+                'id' => $_id,
+                'nome' => $this->nome,
+                'valor' => $this->valor
+            ];
+
+            $statement = $conn->prepare($sql);
+            $statement->execute($data);
+
+            return true;
+        }
+
+    }
+
+?>
